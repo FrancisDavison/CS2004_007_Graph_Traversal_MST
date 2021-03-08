9b25a6ca153077da6b060f04969c16b30db62132
FrancisDavison <francis.nj.davison@gmail.com>
2021-03-08 20:55:26 +0000

    completed example 2 code

--- CS2004_007_Graph_Traversal_MST/src/Lab7.java
+++ CS2004_007_Graph_Traversal_MST/src/Lab7.java
@@ -6,6 +6,10 @@ public class Lab7
 	{
 		double g[][] = {{0,1,2},{1,0,3},{2,3,0}};
 		double mst[][] = MST.PrimsMST(g);
-		System.out.println(Arrays.deepToString(mst));
+		System.out.println("Intro MST: " + Arrays.deepToString(mst));
+		
+		double eg1[][] = {{0,1,2,3,0},{1,0,6,0,5},{2,6,0,4,1},{3,0,4,0,2},{0,5,1,2,0}};
+		double egmst1[][] = MST.PrimsMST(eg1);
+		System.out.println("Example 1 MST: " + Arrays.deepToString(egmst1));
 	}
 }
