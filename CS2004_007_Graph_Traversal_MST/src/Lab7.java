cd4821a05af7c5219a4f4389f27f5779225192db
FrancisDavison <francis.nj.davison@gmail.com>
2021-03-08 20:40:04 +0000

    completed intro code

--- CS2004_007_Graph_Traversal_MST/src/Lab7.java
+++ CS2004_007_Graph_Traversal_MST/src/Lab7.java
@@ -1,7 +1,11 @@
+import java.util.Arrays;
+
 public class Lab7
 {
 	public static void main(String args[])
 	{
-		
+		double g[][] = {{0,1,2},{1,0,3},{2,3,0}};
+		double mst[][] = MST.PrimsMST(g);
+		System.out.println(Arrays.deepToString(mst));
 	}
 }
